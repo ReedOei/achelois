4caed23a338417dd47f289ffa17aeafb25b6b119
Reed Oei <oei.reed@gmail.com>
2018-07-20 19:08:34 -0500

    Bumped version number

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(achelois).
 title('Collection of tools to make writing shell-style scripts with Prolog nicer.').
-version('0.1.5').
+version('0.1.6').
 author('Reed Oei', 'reedoei2@illinois.edu').
 
 home('https://github.com/ReedOei/achelois').
