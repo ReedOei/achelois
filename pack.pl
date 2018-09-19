081da1854e569d55e192389c7ca19c607bb10e39
Reed Oei <oei.reed@gmail.com>
2018-09-19 14:22:04 -0500

    Bumped verson

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(achelois).
 title('Collection of tools to make writing scripts in Prolog easier.').
-version('0.4.3').
+version('0.4.4').
 author('Reed Oei', 'reedoei2@illinois.edu').
 
 home('https://github.com/ReedOei/achelois').
