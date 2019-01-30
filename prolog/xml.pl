5c369eb2ddf4d593a2f7887f9fe41d0b892c3c41
Reed Oei <oei.reed@gmail.com>
2019-01-30 10:46:41 -0600

    Minor documentation updates

--- prolog/xml.pl
+++ prolog/xml.pl
@@ -1,5 +1,6 @@
 :- module(xml, [xml_element/2, modify_element/3]).
 
+% Select an xml element Element from E
 xml_element(Element, E) :-
     is_list(E) ->
         member(Child, E),
@@ -10,6 +11,8 @@ xml_element(Element, element(_Name, _Attrs, Children)) :-
     member(Child, Children),
     xml_element(Element, Child).
 
+% Pred should only match elements that should be modified
+% The first matching element in Element will be modified, and the entire structure returned as NewElement.
 modify_element(Pred, Element, NewElement) :-
     is_list(Element) ->
         select(Child, Element, TempElement),
